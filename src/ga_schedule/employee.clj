04eebb635ac4f62defe0d050621c219d72346d88
carcigenicate <slomifier@gmail.com>
2017-06-30 16:27:40 -0600

    Got some employee code written/

--- src/ga_schedule/employee.clj
+++ src/ga_schedule/employee.clj
@@ -1,4 +1,16 @@
 (ns ga-schedule.employee)
 
 ; TODO: Shifts-unable-to-work
-(defrecord Employee [days-worked-per-period period-days-unavailable])
+(defrecord Employee [perc-days-worked-per-period period-days-unavailable])
+
+(defn can-work-day? [employee period-day]
+  (not
+    ((:period-days-unavailable employee)
+     period-day)))
+
+(defn days-worked-for-period [employee period-length]
+  (* (:perc-days-worked-per-period employee)
+     period-length))
+
+(def test-employee
+  (->Employee 1 #{1 3}))
--- src/ga_schedule/ga.clj
+++ src/ga_schedule/ga.clj
@@ -1,1 +1,7 @@
 (ns ga-schedule.ga)
+
+(defn judge-day [day-shifts employee-map])
+
+
+(defn create-fitness-function [employee-map]
+  (fn [genes]))
